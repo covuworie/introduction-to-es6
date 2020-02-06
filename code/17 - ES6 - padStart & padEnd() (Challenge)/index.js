cd11457725296cd90c34a37400f52958c2c22cc1
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-02-06 13:16:11 -0600

    17 - ES6 - padStart & padEnd() (Challenge)

---
+++ code/17 - ES6 - padStart & padEnd() (Challenge)/index.js
@@ -0,0 +1,6 @@
+let example = 'YouTube.com/CodingTutorials360';
+
+console.log(example.padStart(100));  // adds spaces
+console.log(example.padStart(100).length);
+console.log(example.padEnd(1));  // ignores
+console.log(example.padStart(1).length);
