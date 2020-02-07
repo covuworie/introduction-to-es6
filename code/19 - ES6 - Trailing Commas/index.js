0a249cbc8da5b592129177fde13c7bc5395ff518
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-02-07 17:26:37 -0600

    19 - ES6 - Trailing Commas

---
+++ code/19 - ES6 - Trailing Commas/index.js
@@ -0,0 +1,9 @@
+function add(param1,) {  // trailing comma is perfectly acceptable!
+    const example = {
+        name: 'Dylan',
+    };
+
+    console.log(example);
+};
+
+add(2,);  // and here too!
